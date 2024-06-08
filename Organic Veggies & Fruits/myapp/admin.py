86ff367f9412977fc5168b6439fb78cee2e3e5bd
nevilbhayani <146726616+nevilbhayani@users.noreply.github.com>
2024-06-08 10:57:10 +0530

    main

--- Organic Veggies & Fruits/myapp/admin.py
+++ Organic Veggies & Fruits/myapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(catagory)
-admin.site.register(product)
-admin.site.register(cart)
-admin.site.register(order)
+admin.site.register(Catagory)
+admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(Order)
--- Organic Veggies & Fruits/myapp/models.py
+++ Organic Veggies & Fruits/myapp/models.py
@@ -1,43 +1,58 @@
 from django.db import models
 from django.contrib.auth.models import User 
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+import os
+
 # Create your models here.
 
+class Catagory(models.Model):
+    name = models.CharField(max_length=50)
 
-class catagory(models.Model):
-    cname=models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.cname
+    def _str_(self):
+        return self.name
     
 
-class product(models.Model):
-    catogary=models.ForeignKey(catagory,on_delete=models.SET_NULL,null=True)
-    pname=models.CharField(max_length=30)
-    pprice=models.IntegerField()
-    qty=models.IntegerField()
-    pimg=models.ImageField()
+class Product(models.Model):
+    category = models.ForeignKey(Catagory, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=30)
+    price = models.IntegerField()
+    qty = models.IntegerField()
+    image = models.ImageField(upload_to='products/')
 
-    def __str__(self):
-        return self.pname
+    def _str_(self):
+        return self.name
 
-class cart(models.Model):
-    userid=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
-    pid=models.ForeignKey(product,on_delete=models.SET_NULL,null=True)
-    qty=models.IntegerField()
 
-    def __str__(self):
-        return self.pid.pname,self.pid.qty
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    qty = models.IntegerField()
 
-class order(models.Model):
-    userid=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
-    cname=models.CharField(max_length=30)
-    pname=models.CharField(max_length=30)
-    pprice=models.IntegerField()
-    qty=models.IntegerField()
-    pimg=models.ImageField()
+    def _str_(self):
+        return f"{self.product.name} - {self.qty}"
 
-    def __str__(self):
-        return self.pname
-    
-    def __str__(self):
-        return self.cname
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    price = models.IntegerField()
+    qty = models.IntegerField()
+    img = models.ImageField(upload_to='orders/')
+
+    def _str_(self):
+        return self.name
+
+
+@receiver(post_delete, sender=Product)
+def delete_product_image(sender, instance, **kwargs):
+    if instance.img:
+        if os.path.isfile(instance.img.path):
+            os.remove(instance.img.path)
+
+@receiver(post_delete, sender=Order)
+def delete_order_image(sender, instance, **kwargs):
+    if instance.img:
+        if os.path.isfile(instance.img.path):
+            os.remove(instance.img.path)
